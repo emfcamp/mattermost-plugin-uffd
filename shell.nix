e000fc18a88ff2f419b57076b79e687d89930d40
Luke Granger-Brown <git@lukegb.com>
2025-07-27 14:52:44 +0100

    add shell.nix to make my life easier

---
+++ shell.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    gnumake
+    go
+
+    # Useful for doing... admin stuff
+    mmctl
+
+    # The Node Dot J S ecosystem
+    nodejs
+    python3
+    stdenv
+    pkg-config
+
+    pixman  # node-canvas
+    cairo  # node-canvas
+    librsvg  # node-canvas
+    libjpeg  # node-canvas
+    pango  # node-canvas
+  ];
+
+  shellHook = ''
+    test -e $HOME/.cache/mmlocal.sock && export MM_LOCALSOCKETPATH=$HOME/.cache/mmlocal.sock
+    test -e /var/tmp/mattermost_local.socket && export MM_LOCALSOCKETPATH=/var/tmp/mattermost_local.socket
+
+    if [[ ! -z "''${MM_LOCALSOCKETPATH}" ]]; then
+      export MMCTL_LOCAL=true
+      export MMCTL_LOCAL_SOCKET_PATH=''${MM_LOCALSOCKETPATH}
+    fi
+
+    echo "Set MM_SERVICESETTINGS_ENABLEDEVELOPER=1 to build only for local arch"
+    echo
+    echo "Useful commands:"
+    echo " - make"
+    echo " - make test"
+    echo " - make watch"
+    echo
+  '';
+}
