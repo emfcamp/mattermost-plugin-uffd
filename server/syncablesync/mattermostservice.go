059fca5750b75cedbe175923d94e26cc51cfa9e2
Luke Granger-Brown <git@lukegb.com>
2025-09-02 17:39:58 +0100

    add emf-leads

--- server/syncablesync/mattermostservice.go
+++ server/syncablesync/mattermostservice.go
@@ -305,6 +305,10 @@ func (m *Mattermost) FetchGroupsAndSyncables(ctx context.Context) ([]Group, erro
 			allUsersSyncables = append(allUsersSyncables, Syncable{
 				Target: t,
 			})
+		case "emf-leads":
+			leadSyncables = append(leadSyncables, Syncable{
+				Target: t,
+			})
 		}
 	}
 	groups = append(groups, Group{
